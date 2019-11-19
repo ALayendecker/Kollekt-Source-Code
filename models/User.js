3f7354511229c364ac1eaabe56f7d26cfc63eeab
ALayendecker <alcommits@gmail.com>
2019-11-19 13:44:39 -0600

    Startin user auth

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  collections: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Collection"
+    }
+  ],
+
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const connectDB = require("./config/db");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+//connect to database
+connectDB();
+
+// initialize middleware
+app.use(express.json({ extended: false }));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,14 +20,20 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
   console.log("hit 3001");
+  res.send("API Running");
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`server started on port ${PORT}!`);
 });
