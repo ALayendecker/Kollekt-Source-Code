45c4e2fbbec56d873aaa32021739b965d387a7ae
Kala Elam <speechgirl1505@gmail.com>
2019-11-21 16:01:13 -0600

    im a classy

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./style.css";
 function Nav() {
   return (
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
       <img src="/assets/images/logo_small.png"></img>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         type="button"
         data-toggle="collapse"
         data-target="#navbarNav"
@@ -13,27 +13,27 @@ function Nav() {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon "></span>
+        <span className="navbar-toggler-icon "></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link " href="/">
-              Home <span class="sr-only">(current)</span>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <a className="nav-link " href="/">
+              Home <span className="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/login">
+          <li className="nav-item">
+            <a className="nav-link" href="/login">
               Sign-in
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/signup">
+          <li className="nav-item">
+            <a className="nav-link" href="/signup">
               Sign-up
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/dashboard">
+          <li className="nav-item">
+            <a className="nav-link" href="/dashboard">
               Dashboard
             </a>
           </li>
