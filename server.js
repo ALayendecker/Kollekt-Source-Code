f0c3e06f3ca45f857e2c19940f15c9652752aff4
ALayendecker <alcommits@gmail.com>
2019-12-04 14:30:48 -0600

    script change build change

--- server.js
+++ server.js
@@ -24,16 +24,16 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
-  app.get("/*", function(request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
-}
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
+// app.get("/*", function(request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
+// }
 // Send every other request to the React app
 // Define any API routes before this runs
 // app.get("*", (req, res) => {
