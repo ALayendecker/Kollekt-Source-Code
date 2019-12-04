48b0aeaafbd2fe0c01c19c0d371a95c7d5ff8ca7
ALayendecker <alcommits@gmail.com>
2019-12-04 14:04:07 -0600

    production change

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-if (process.env.NODE_ENV === ".env") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   // app.get("*", (req, res) => {
