7026d796770fbb5db76589f27aff6a01da945f12
Alayen <alcommits@gmail.com>
2019-12-04 22:30:23 -0600

    i changed the vars and it broke the build changing prod to dev

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(express.static("client/build"));
   app.get("*", function(request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
