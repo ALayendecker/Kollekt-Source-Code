12f7ecf8a3d0d498e82ecdf7545d1e00a69eabc9
Alayen <alcommits@gmail.com>
2019-12-05 10:12:53 -0600

    if else in server.js

--- server.js
+++ server.js
@@ -20,10 +20,14 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  // app.get("*", function(request, response) {
-  //   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  });
 } else if (process.env.NODE_ENV === "development") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 3001;
