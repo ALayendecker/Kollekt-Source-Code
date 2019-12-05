db6d98c263d4277163510d5df94b0b753d5cfd7e
Alayen <alcommits@gmail.com>
2019-12-04 22:13:29 -0600

    testing swap to production

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/*", function(request, response) {
+  app.get("*", function(request, response) {
     response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
